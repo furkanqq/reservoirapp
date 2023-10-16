e1c61cb1f60bd980b974773d14f00012457a2360
furkanqq <133240374+furkanqq@users.noreply.github.com>
2023-10-16 15:22:57 +0300

    dede

--- src/App.js
+++ src/App.js
@@ -15,41 +15,41 @@ import { createWalletClient, http, custom } from "viem";
 import { ListModal } from "@reservoir0x/reservoir-kit-ui";
 
 function App() {
-  const {
-    data: listings1,
-    fetchNextPage,
-    hasNextPage,
-  } = useListings({
-    contracts: ["0x355b3cA2B5e8eA04e65C41b0EA73a88C4f39AC9a"],
-  });
+  // const {
+  //   data: listings1,
+  //   fetchNextPage,
+  //   hasNextPage,
+  // } = useListings({
+  //   contracts: ["0x355b3cA2B5e8eA04e65C41b0EA73a88C4f39AC9a"],
+  // });
 
-  const { data: tokens } = useTokens({
-    tokens: ["0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb:1"],
-  });
+  // const { data: tokens } = useTokens({
+  //   tokens: ["0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb:1"],
+  // });
 
-  const { data: collection } = useCollections({
-    id: "0xa6551db26e8f54C7707A17Bd993a029592C0e4BB",
-    // slug: "doubleeagle",
-  });
+  // const { data: collection } = useCollections({
+  //   id: "0xa6551db26e8f54C7707A17Bd993a029592C0e4BB",
+  //   // slug: "doubleeagle",
+  // });
 
-  const { data: collectionActivity } = useCollectionActivity(
-    "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb"
-  );
+  // const { data: collectionActivity } = useCollectionActivity(
+  //   "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb"
+  // );
 
-  const { data: userActivity } = useUsersActivity([
-    "0x355b3cA2B5e8eA04e65C41b0EA73a88C4f39AC9a",
-  ]);
+  // const { data: userActivity } = useUsersActivity([
+  //   "0x355b3cA2B5e8eA04e65C41b0EA73a88C4f39AC9a",
+  // ]);
 
-  const { data: tokenActivity } = useTokenActivity(
-    "0x744df993f93c89801cadbea8a4a3fd2b4a443d2c:1793"
-  );
+  // const { data: tokenActivity } = useTokenActivity(
+  //   "0x744df993f93c89801cadbea8a4a3fd2b4a443d2c:1793"
+  // );
 
-  console.log("listings1", listings1);
-  console.log("tokens", tokens);
-  console.log("collection", collection);
-  console.log("collectionActivity", collectionActivity);
-  console.log("userActivity", userActivity);
-  console.log("tokenActivity", tokenActivity);
+  // console.log("listings1", listings1);
+  // console.log("tokens", tokens);
+  // console.log("collection", collection);
+  // console.log("collectionActivity", collectionActivity);
+  // console.log("userActivity", userActivity);
+  // console.log("tokenActivity", tokenActivity);
 
   const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect();
@@ -61,11 +61,17 @@ function App() {
       account: address,
       transport: custom(window.ethereum),
     });
-  
+
     getClient()?.actions.buyToken({
       items: [
-        { token: "0x068fc19f85605336a75b87dad04acf8fa4f92e85:618", quantity: 1 },
+        {
+          token: "0x068FC19F85605336A75b87DAd04acF8fA4F92E85:25",
+          quantity: 1,
+        },
       ],
+      feesOnTopBps: ["0x33905203045DFA61C62451fe9516F4654a6517CB:100"], 
+      // feesOnTopUsdt: ["0x33905203045DFA61C62451fe9516F4654a6517CB:0.01"], 
+
       wallet,
       onProgress: (steps) => {
         console.log(steps);
@@ -73,9 +79,33 @@ function App() {
     });
   };
 
+  const listingMe = () => {
+    const wallet = createWalletClient({
+      account: address,
+      transport: http(),
+    });
+
+    getClient()?.actions.listToken({
+      listings: [
+        {
+          token: "0x068fc19f85605336a75b87dad04acf8fa4f92e85:1140",
+          weiPrice: 10000000000000000,
+          orderbook: "reservoir",
+          orderKind: "seaport-v1.5",
+          expirationTime: 1667403808616,
+        },
+      ],
+      wallet,
+      onProgress: (steps) => {
+        console.log(steps);
+      },
+    });
+  };
+  
   return (
     <div className="App">
       <button onClick={buyMe}>Buy Me!</button>
+      <button onClick={listingMe}>Listing Me!</button>
       <div>
         {connectors.map((connector) => (
           <button
